75aeddef02b04b6e5ede281585392ed36916f316
ravisharma <rssharmakapro@gmail.com>
2023-07-23 15:40:26 +0530

    service section

--- client/src/Pages/Home/AboutUs.jsx
+++ client/src/Pages/Home/AboutUs.jsx
@@ -6,27 +6,29 @@ const AboutUs = () => {
 
 
 return (
-    <Box background={'#6EC1E4'}>
+    <Box background={'#edfaffa1'} pt={20}>
       <Flex>
-        <Box>
-        <Image position={'absolute'} src='https://demo.rstheme.com/wordpress/tekone/wp-content/uploads/2023/05/about-drone-img-h5.png'/>
-            <Image src='https://demo.rstheme.com/wordpress/tekone/wp-content/uploads/2023/05/about-main-img-h5.png'/>
+        <Box w={'50%'}>
+        <Image w={180} left={350} mt={20} position={'absolute'} src='https://demo.rstheme.com/wordpress/tekone/wp-content/uploads/2023/05/about-drone-img-h5.png'/>
+            <Image  src='https://demo.rstheme.com/wordpress/tekone/wp-content/uploads/2023/05/about-main-img-h5.png'/>
             
         </Box>
-        <Box>
-            <Text>ABOUT US</Text>
-            <Text fontSize='36px' lineHeight='45px' color={'#323F7C'} fontFamily={'"Space Grotesk", sans-serif'} fontWeight={'bold'}>We Believe That Every Problem Has A Solution</Text>
-            <Text>Deliver beautiful emails that get opened and clicked every time. Personalized content to every subscriber. Fully customizable templates. Build relationships that lead to conversions.</Text>
-            <Flex>
-            <Flex><BsBookmarkCheckFill/> <Text>Internal Networking</Text></Flex>
-            <Flex><BsBookmarkCheckFill/> <Text>Internal Networking</Text></Flex>
+        <Box w={'45%'}>
+            <Text fontSize={'16px'} fontWeight='500' lineHeight={'26px'} color={'#5944E8'} fontFamily={'"Space Grotesk", sans-serif'}>ABOUT US</Text>
+            <Text fontSize='36px' lineHeight='45px' color={'#323F7C'} fontFamily={'"Space Grotesk", sans-serif'} fontWeight={'bold'} mt={2} mb={4}>We Believe That Every Problem Has A Solution</Text>
+            <Text fontSize={'17px'} lineHeight={'26px'} mb={5} fontFamily={'"Open Sans", sans-serif'} fontWeight='520' color={'#606060'}>Deliver beautiful emails that get opened and clicked every time. Personalized content to every subscriber. Fully customizable templates. Build relationships that lead to conversions.</Text>
+            <Text fontSize={'26px'} fontWeight={'600'} lineHeight={'26px'} fontFamily={'"Space Grotesk", sans-serif'} color='#112437' mb={5}> <u> Top Features </u></Text>
+            <Flex mt={5} justifyContent={'space-around'}>
+            <Flex gap={5}><BsBookmarkCheckFill/> <Text>Internal Networking</Text></Flex>
+            <Flex gap={5}><BsBookmarkCheckFill/> <Text>Internal Networking</Text></Flex>
             </Flex>
-            <Flex>
-            <Flex><BsBookmarkCheckFill/> <Text>Internal Networking</Text></Flex>
-            <Flex><BsBookmarkCheckFill/> <Text>Internal Networking</Text></Flex>
+            <Flex mt={3} mb={10} justifyContent={'space-around'}>
+            <Flex gap={5}><BsBookmarkCheckFill/> <Text>Internal Networking</Text></Flex>
+            <Flex gap={5}><BsBookmarkCheckFill/> <Text>Internal Networking</Text></Flex>
             </Flex>
             <Text>Rring to the table win-win survival strategies to ensure proactive domination. At the end of the day going forward.</Text>
-            <Button>Discover More</Button>
+            <Button mt={5} backgroundImage={'linear-gradient(99.43deg, #513DE8 0.99%, #8366E3 99.43%)'}
+            borderRadius={'30px'} padding='21px 50px 21px 50px' color={'white'} fontSize={'16px'} fontWeight='500' lineHeight={'22px'} _hover={{color:'white'}}>Discover More</Button>
         </Box>
       </Flex>
     </Box>
--- client/src/Pages/Home/ServicesSection.jsx
+++ client/src/Pages/Home/ServicesSection.jsx
@@ -5,29 +5,40 @@ const ServicesSection = () => {
 
 
 return(
-    <Box>
-        <Heading>Our Services</Heading>
-        <Flex gap={'40px'} w='90%' m='auto' justifyContent='space-around'>
-            <Box border='2px solid grey' borderRadius={20}>
+    <Box  background={'linear-gradient(0deg, rgba(242,242,242,1) 2%, rgba(242,242,242,1) 12%, rgba(242,242,242,1) 30%, rgba(255,255,255,1) 100%)'}>
+         <Flex justifyContent={'center'} mt={20} pt={10}>
+        <Image src='https://demo.rstheme.com/wordpress/tekone/wp-content/uploads/2023/03/heart_beat_graph_dark.svg'/>
+        <Text color={'#323F7C'} fontSize={'20px'} fontWeight={'700'} fontFamily={'"Space Grotesk", Sans-serif'}>Our Feachered Services</Text>
+        <Image src='https://demo.rstheme.com/wordpress/tekone/wp-content/uploads/2023/03/heart_beat_graph_dark.svg'/>
+      </Flex>
+        <Text fontSize={'36px'} fontFamily={'"Space Grotesk", sans-serif'} letterSpacing={0} fontWeight='600' textAlign={'center'} color={'#002559'} mt={3} mb={10}>We run all kinds of IT services</Text>
+        <Flex gap={'40px'} w='90%' m='auto' mb='40px' justifyContent='space-around'>
+            <Box  background='white' borderRadius={20}>
                 <Image borderTopRadius={20} src='https://demo.rstheme.com/wordpress/tekone/wp-content/uploads/2023/03/service4-image-1.png'/>
-                <Box ml={'20px'} border='2px solid blue' mt='-40px' position='absolute' borderRadius='50%' w={20} h={20}></Box>
+                <Box ml={'20px'} background={'#F79C53'} mt='-40px' position='absolute' borderRadius='50%' _hover={{background:'linear-gradient(135deg,rgb(2,3,129) 0%,rgb(40,116,252) 100%)'}} w={20} h={20}>
+                    <Image padding={'15px'} h='auto' verticalAlign={'middle'} borderRadius='50%' src='https://demo.rstheme.com/wordpress/tekone/wp-content/uploads/2023/03/cyber-icon.png'/>
+                </Box>
                 <Box w='90%' m='auto'>
                 <Text fontSize='24px' fontFamily={'"Space Grotesk", sans-serif'} color={'#002559'} fontWeight={'bold'} lineHeight={'36px'} mt={'60px'}>Tagline Here</Text>
                 <Text fontWeight='bold' color='#606060' fontFamily={'"Open Sans", sans-serif'} fontSize={'16px'} lineHeight={'28px'} mt={'20px'}>Bring to the table win-win survival strategies to ensure proactive domination.</Text>
                 <Button mb={'10px'} mt={'20px'}>Read More</Button>
                 </Box>
             </Box>
-            <Box border='2px solid grey' borderRadius={20}>
+            <Box  background='white' borderRadius={20}>
                 <Image borderTopRadius={20} src='https://demo.rstheme.com/wordpress/tekone/wp-content/uploads/2023/03/service4-image-3.png'/>
-                <Box ml={'20px'} border='2px solid blue' mt='-40px' position='absolute' borderRadius='50%' w={20} h={20}></Box>
+                <Box ml={'20px'} background={'#F79C53'} mt='-40px' position='absolute' borderRadius='50%' _hover={{background:'linear-gradient(135deg,rgb(2,3,129) 0%,rgb(40,116,252) 100%)'}} w={20} h={20}>
+                    <Image padding={'15px'} h='auto' verticalAlign={'middle'} borderRadius='50%' src='https://demo.rstheme.com/wordpress/tekone/wp-content/uploads/2023/03/cyber-icon.png'/>
+                </Box>
                 <Box w='90%' m='auto'>
                 <Text fontSize='24px' fontFamily={'"Space Grotesk", sans-serif'} color={'#002559'} fontWeight={'bold'} lineHeight={'36px'} mt={'60px'}>Tagline Here</Text>
                 <Text fontWeight='bold' color='#606060' fontFamily={'"Open Sans", sans-serif'} fontSize={'16px'} lineHeight={'28px'} mt={'20px'}>Bring to the table win-win survival strategies to ensure proactive domination.</Text>
                 <Button mb={'10px'} mt={'20px'}>Read More</Button>
                 </Box>
-            </Box><Box border='2px solid grey' borderRadius={20}>
+            </Box><Box  background='white' borderRadius={20}>
                 <Image borderTopRadius={20} src='https://demo.rstheme.com/wordpress/tekone/wp-content/uploads/2023/03/service4-image-2.png'/>
-                <Box ml={'20px'} border='2px solid blue' mt='-40px' position='absolute' borderRadius='50%' w={20} h={20}></Box>
+                <Box ml={'20px'} background={'#F79C53'} mt='-40px' position='absolute' borderRadius='50%' _hover={{background:'linear-gradient(135deg,rgb(2,3,129) 0%,rgb(40,116,252) 100%)'}} w={20} h={20}>
+                    <Image padding={'15px'} h='auto' verticalAlign={'middle'} borderRadius='50%' src='https://demo.rstheme.com/wordpress/tekone/wp-content/uploads/2023/03/cyber-icon.png'/>
+                </Box>
                 <Box w='90%' m='auto'>
                 <Text fontSize='24px' fontFamily={'"Space Grotesk", sans-serif'} color={'#002559'} fontWeight={'bold'} lineHeight={'36px'} mt={'60px'}>Tagline Here</Text>
                 <Text fontWeight='bold' color='#606060' fontFamily={'"Open Sans", sans-serif'} fontSize={'16px'} lineHeight={'28px'} mt={'20px'}>Bring to the table win-win survival strategies to ensure proactive domination.</Text>
@@ -37,27 +48,33 @@ return(
         </Flex>
       
 
-        <Flex gap={'40px'} w='90%' m='auto' justifyContent='space-around'>
-            <Box border='2px solid grey' borderRadius={20}>
+        <Flex gap={'40px'} w='90%' m='auto' pb={20} justifyContent='space-around'>
+            <Box  background='white' borderRadius={20}>
                 <Image borderTopRadius={20} src='https://demo.rstheme.com/wordpress/tekone/wp-content/uploads/2023/03/service4-image-1.png'/>
-                <Box ml={'20px'} border='2px solid blue' mt='-40px' position='absolute' borderRadius='50%' w={20} h={20}></Box>
+                <Box ml={'20px'} background={'#F79C53'} mt='-40px' position='absolute' borderRadius='50%' _hover={{background:'linear-gradient(135deg,rgb(2,3,129) 0%,rgb(40,116,252) 100%)'}} w={20} h={20}>
+                    <Image padding={'15px'} h='auto' verticalAlign={'middle'} borderRadius='50%' src='https://demo.rstheme.com/wordpress/tekone/wp-content/uploads/2023/03/cyber-icon.png'/>
+                </Box>
                 <Box w='90%' m='auto'>
                 <Text fontSize='24px' fontFamily={'"Space Grotesk", sans-serif'} color={'#002559'} fontWeight={'bold'} lineHeight={'36px'} mt={'60px'}>Tagline Here</Text>
                 <Text fontWeight='bold' color='#606060' fontFamily={'"Open Sans", sans-serif'} fontSize={'16px'} lineHeight={'28px'} mt={'20px'}>Bring to the table win-win survival strategies to ensure proactive domination.</Text>
                 <Button mb={'10px'} mt={'20px'}>Read More</Button>
                 </Box>
             </Box>
-            <Box border='2px solid grey' borderRadius={20}>
+            <Box  background='white' borderRadius={20}>
                 <Image borderTopRadius={20} src='https://demo.rstheme.com/wordpress/tekone/wp-content/uploads/2023/03/service4-image-3.png'/>
-                <Box ml={'20px'} border='2px solid blue' mt='-40px' position='absolute' borderRadius='50%' w={20} h={20}></Box>
+                <Box ml={'20px'} background={'#F79C53'} mt='-40px' position='absolute' borderRadius='50%' _hover={{background:'linear-gradient(135deg,rgb(2,3,129) 0%,rgb(40,116,252) 100%)'}} w={20} h={20}>
+                    <Image padding={'15px'} h='auto' verticalAlign={'middle'} borderRadius='50%' src='https://demo.rstheme.com/wordpress/tekone/wp-content/uploads/2023/03/cyber-icon.png'/>
+                </Box>
                 <Box w='90%' m='auto'>
                 <Text fontSize='24px' fontFamily={'"Space Grotesk", sans-serif'} color={'#002559'} fontWeight={'bold'} lineHeight={'36px'} mt={'60px'}>Tagline Here</Text>
                 <Text fontWeight='bold' color='#606060' fontFamily={'"Open Sans", sans-serif'} fontSize={'16px'} lineHeight={'28px'} mt={'20px'}>Bring to the table win-win survival strategies to ensure proactive domination.</Text>
                 <Button mb={'10px'} mt={'20px'}>Read More</Button>
                 </Box>
-            </Box><Box border='2px solid grey' borderRadius={20}>
+            </Box><Box  background='white' borderRadius={20}>
                 <Image borderTopRadius={20} src='https://demo.rstheme.com/wordpress/tekone/wp-content/uploads/2023/03/service4-image-2.png'/>
-                <Box ml={'20px'} border='2px solid blue' mt='-40px' position='absolute' borderRadius='50%' w={20} h={20}></Box>
+                <Box ml={'20px'} background={'#F79C53'} mt='-40px' position='absolute' borderRadius='50%' _hover={{background:'linear-gradient(135deg,rgb(2,3,129) 0%,rgb(40,116,252) 100%)'}} w={20} h={20}>
+                    <Image padding={'15px'} h='auto' verticalAlign={'middle'} borderRadius='50%' src='https://demo.rstheme.com/wordpress/tekone/wp-content/uploads/2023/03/cyber-icon.png'/>
+                </Box>
                 <Box w='90%' m='auto'>
                 <Text fontSize='24px' fontFamily={'"Space Grotesk", sans-serif'} color={'#002559'} fontWeight={'bold'} lineHeight={'36px'} mt={'60px'}>Tagline Here</Text>
                 <Text fontWeight='bold' color='#606060' fontFamily={'"Open Sans", sans-serif'} fontSize={'16px'} lineHeight={'28px'} mt={'20px'}>Bring to the table win-win survival strategies to ensure proactive domination.</Text>
